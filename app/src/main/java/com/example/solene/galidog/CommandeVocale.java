af4ccefbdd33042ebe51907a78544f7422e2a7bc
soleneG02 <46197495+soleneG02@users.noreply.github.com>
2019-01-12 19:16:46 +0100

    Modification forme CommandeVocale

--- app/src/main/java/com/example/solene/galidog/CommandeVocale.java
+++ app/src/main/java/com/example/solene/galidog/CommandeVocale.java
@@ -1,11 +1,32 @@
 package com.example.solene.galidog;
 
+import com.google.android.gms.maps.model.LatLng;
+
 public class CommandeVocale {
 
     //private ? commande
-    private int idCommande;
+    private int idCommande = 0;
+    private LatLng coordonnees;
 
-    public CommandeVocale() {
+    public CommandeVocale(){
+        this.idCommande ++;
+    }
+
+    public CommandeVocale(String direction, LatLng coord) {
+        super();
+        this.coordonnees = coord;
+        if (direction == "D") {
+            //commande = droite
+        }
+        else if (direction == "G") {
+            //commande = gauche
+        }
+        else if (direction == "H") {
+            //commande = halte
+        }
+        else if (direction == "A") {
+            //commande = autre
+        }
     }
 
     public int getIdCommande() {
@@ -14,5 +35,13 @@ public class CommandeVocale {
 
     public void setIdCommande(int indiceCommande) {
         this.idCommande = indiceCommande;
+    }
+
+    public LatLng getCoordonnees() {
+        return coordonnees;
+    }
+
+    public void setCoordonnees(LatLng coordonnees) {
+        this.coordonnees = coordonnees;
     }
 }

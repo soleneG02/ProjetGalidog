b5a5f06fcaf9257a2ee597857f73f5e22902d976
soleneG02 <46197495+soleneG02@users.noreply.github.com>
2019-01-15 12:46:28 +0100

    MainActivity - 15/01

--- app/src/main/java/com/example/solene/galidog/MainActivity.java
+++ app/src/main/java/com/example/solene/galidog/MainActivity.java
@@ -31,11 +31,21 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+        //Vérifier qu'on a des données
         btnOldPath.setOnClickListener(new View.OnClickListener() {
             @Override
             /* L'utilisateur souhaite accéder au chemin enregistré, accès à l'activité correspondante */
             public void onClick(View v) {
                 Intent intentOldMapsActivity = new Intent(MainActivity.this, OldMapsActivity.class);
+
+                Bundle bundle = getIntent().getExtras();
+                TransfertDonnees donnees = bundle.getParcelable("données");
+                    // donnees.getListePonts..
+
+                Bundle bundle2 = new Bundle();
+                bundle.putParcelable("donnees", donnees);
+                intentOldMapsActivity.putExtras(bundle2);
+
                 startActivity(intentOldMapsActivity);
             }
         });

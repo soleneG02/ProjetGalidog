8e0bb29cc6aca2c377762567fc2a62a5362754f7
lealrs <46625493+lealrs@users.noreply.github.com>
2019-01-15 09:50:42 +0100

    Update OldMapsActivity.java

--- app/src/main/java/com/example/solene/galidog/OldMapsActivity.java
+++ app/src/main/java/com/example/solene/galidog/OldMapsActivity.java
@@ -2,6 +2,20 @@ package com.example.solene.galidog;
 
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.app.FragmentActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Gravity;
+import android.view.View;
+import android.widget.Toast;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -10,9 +24,13 @@ import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class OldMapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
     private GoogleMap mMap;
+    private Button btnStartPath;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,6 +40,8 @@ public class OldMapsActivity extends FragmentActivity implements OnMapReadyCallb
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
+
+        btnStartPath = (Button) findViewById(R.id.activity_main_btn_start_path);
     }
 
 
@@ -38,9 +58,156 @@ public class OldMapsActivity extends FragmentActivity implements OnMapReadyCallb
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
 
-        // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        androidFirstLocation();
+
+        /* début du trajet */
+        btnStartPath.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast toast = Toast.makeText(OldMapsActivity.this, "Le trajet démarre", Toast.LENGTH_SHORT);
+                toast.setGravity(Gravity.CENTER, 0, 0);
+                toast.show();
+                androidUpdateLocation();
+                btnStartPath.setText("Arrêter le trajet");
+                btnStartPath.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        onPause();
+                        Toast.makeText(OldMapsActivity.this, "Trajet termminé", Toast.LENGTH_SHORT).show();
+                        btnStartPath.setText("Retour à l'accueil");
+                        btnStartPath.setOnClickListener(new View.OnClickListener() {
+                            @Override
+                            public void onClick(View v) {
+                                Intent retourAccueil = new Intent(OldMapsActivity.this, MainActivity.class);
+                                startActivity(retourAccueil);
+                            }
+                        });
+
+                    }
+                });
+
+            }
+        });
+
     }
+
+
+    private LocationManager androidLocationManager;
+    private LocationListener androidLocationListener;
+    private final static int REQUEST_CODE_UPDATE_LOCATION = 42;
+    private boolean commandeCreee = false;
+    private int compteurCommande = 0;
+    private Point pointSuivant;
+    private List<Point> listePoints = new ArrayList<>();
+    private List<CommandeVocale> listeCommandes = new ArrayList<>();
+
+
+    public void androidFirstLocation() {
+        /*
+        Cette fonction affiche la géolocalisation de l'utilisateur lorsqu'il arrive pour la première fois sur la page.
+         */
+        if (ActivityCompat.checkSelfPermission(OldMapsActivity.this, Manifest.permission.ACCESS_FINE_LOCATION
+        ) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(
+                    OldMapsActivity.this,
+                    new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
+                    REQUEST_CODE_UPDATE_LOCATION);
+        } else {
+            androidLocationManager = (LocationManager) this.getSystemService(OldMapsActivity.this.LOCATION_SERVICE);
+            androidLocationListener = new LocationListener() {
+                public void onLocationChanged(Location loc) {
+                    /* Affichage des coordonnées & création d'un marqueur */
+                    double latNow = loc.getLatitude();
+                    double lonNow = loc.getLongitude();
+                    Toast.makeText(OldMapsActivity.this, "Coordonnées : " + latNow + " / " + lonNow, Toast.LENGTH_SHORT).show();
+                    LatLng youAreHere = new LatLng(latNow, lonNow);
+                    Point newPoint = new Point(youAreHere, -1);
+                    listePoints.add(newPoint);
+                    mMap.addMarker(new MarkerOptions().position(youAreHere).title("Vous êtes ici"));
+                    int padding = 15;
+                    mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(youAreHere, padding));
+                }
+
+                public void onStatusChanged(String provider, int status, Bundle extras) {
+                }
+
+                public void onProviderEnabled(String provider) {
+                }
+
+                public void onProviderDisabled(String provider) {
+                }
+            };
+
+            /* Requête unique (première géolocalisation) */
+            androidLocationManager.requestSingleUpdate(
+                    LocationManager.GPS_PROVIDER,
+                    androidLocationListener,
+                    null);
+        }
+    }
+
+    public void androidUpdateLocation() {
+        /*
+        cette fonction actualise la position et l'affiche au fur et à mesure que l'utilisateur avance */
+
+        if (ActivityCompat.checkSelfPermission(OldMapsActivity.this, Manifest.permission.ACCESS_FINE_LOCATION
+        ) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(
+                    OldMapsActivity.this,
+                    new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
+                    REQUEST_CODE_UPDATE_LOCATION);
+        } else {
+            androidLocationManager = (LocationManager) this.getSystemService(OldMapsActivity.this.LOCATION_SERVICE);
+            androidLocationListener = new LocationListener() {
+                public void onLocationChanged(Location loc) {
+                    // Récupération de la localisation
+                    double latNow = loc.getLatitude();
+                    double lonNow = loc.getLongitude();
+                    LatLng youAreHere = new LatLng(latNow, lonNow);
+
+                    listePoints.add(pointSuivant);
+
+                }
+                public void onStatusChanged(String provider, int status, Bundle extras) {}
+                public void onProviderEnabled(String provider) {}
+                public void onProviderDisabled(String provider) {}
+            };
+
+
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String permissions[], int[] grantResults) {
+        /*
+        Cette fonction vérifie que le téléphone possède bien les autorisations pour capter la localisation.
+         */
+        switch (requestCode) {
+            case REQUEST_CODE_UPDATE_LOCATION:
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    androidFirstLocation();
+                    androidUpdateLocation();
+                } else {
+                    Toast.makeText(OldMapsActivity.this, "Permission refusée.", Toast.LENGTH_LONG).show();
+                }
+                return;
+        };
+    }
+
+    @Override
+    protected void onPause() {
+        /*
+        Cette fonction stoppe les fonctions en cours en cas de changement d'orientation ou de fermeture de l'application.
+         */
+        super.onPause();
+        if (androidLocationListener != null) {
+            if (androidLocationManager == null) {
+                androidLocationManager = (LocationManager) this.getSystemService(OldMapsActivity.this.LOCATION_SERVICE);
+            }
+            androidLocationManager.removeUpdates(androidLocationListener);
+            androidLocationManager = null;
+            androidLocationListener = null;
+        }
+    }
+
 }

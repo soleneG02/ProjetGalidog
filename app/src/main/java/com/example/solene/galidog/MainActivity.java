9d2f9807862e86ddda66715001642fcb8ae2a661
soleneG02 <46197495+soleneG02@users.noreply.github.com>
2019-01-16 09:29:33 +0100

    Update MainActivity.java

--- app/src/main/java/com/example/solene/galidog/MainActivity.java
+++ app/src/main/java/com/example/solene/galidog/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.solene.galidog;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -17,43 +18,50 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Intent intent = getIntent();
+        if (intent != null){
 
-        textStart = (TextView) findViewById(R.id.activity_main_text_start);
-        btnNewPath = (Button) findViewById(R.id.activity_main_btn_new);
-        btnOldPath = (Button) findViewById(R.id.activity_main_btn_old);
+            textStart = (TextView) findViewById(R.id.activity_main_text_start);
+            btnNewPath = (Button) findViewById(R.id.activity_main_btn_new);
+            btnOldPath = (Button) findViewById(R.id.activity_main_btn_old);
 
-        btnNewPath.setOnClickListener(new View.OnClickListener() {
-            @Override
-            /* L'utilisateur souhaite enregistrer un nouveau chemin, accès à l'activité correspondante */
-            public void onClick(View v) {
-                Intent intentMapsActivity = new Intent(MainActivity.this, MapsActivity.class);
-                startActivity(intentMapsActivity);
-            }
-        });
-
-
-        final Intent intentOldMapsActivity = new Intent(MainActivity.this, OldMapsActivity.class);
-        Bundle bundle = getIntent().getExtras();
-        TransfertDonnees donnees = bundle.getParcelable("données");
-
-        Bundle bundle2 = new Bundle();
-        bundle.putParcelable("donnees", donnees);
-        intentOldMapsActivity.putExtras(bundle2);
-
-        if (intentOldMapsActivity.getParcelableExtra("donnees") != null) {
-            btnOldPath.setSaveEnabled(true);
-            // A TESTER !!! Le bouton doit être grisé si on a pas déja enregistré un chemin
-
-            btnOldPath.setOnClickListener(new View.OnClickListener() {
+            btnNewPath.setOnClickListener(new View.OnClickListener() {
                 @Override
-                /* L'utilisateur souhaite accéder au chemin enregistré, accès à l'activité correspondante */
+                /* L'utilisateur souhaite enregistrer un nouveau chemin, accès à l'activité correspondante */
                 public void onClick(View v) {
-
-
-                    startActivity(intentOldMapsActivity);
+                    Intent intentMapsActivity = new Intent(MainActivity.this, MapsActivity.class);
+                    startActivity(intentMapsActivity);
                 }
             });
+
+            if (intent.hasExtra("donnees")){
+                Bundle bundle = getIntent().getExtras();
+                final TransfertDonnees donnees = bundle.getParcelable("donnees");
+
+                Log.i("salut", "salut, je suis rentré dans le if");
+
+                btnOldPath.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    /* L'utilisateur souhaite accéder au chemin enregistré, accès à l'activité correspondante */
+                    public void onClick(View v) {
+                        Log.i("salut", "salut, je suis rentré dans l'appel à l'activité");
+                        Intent intentOldMapsActivity = new Intent(MainActivity.this, OldMapsActivity.class);
+                        Bundle bundle = new Bundle();
+                        bundle.putParcelable("donnees", donnees);
+                        intentOldMapsActivity.putExtras(bundle);
+                        startActivity(intentOldMapsActivity);
+                    }
+                });
+            }
+
+
+
+
+
         }
 
-    }
+        }
+
+
+
 }

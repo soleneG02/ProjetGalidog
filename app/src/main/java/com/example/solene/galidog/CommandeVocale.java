acdd1f7ea1d7e929ea50f61c7accef673e5c6264
soleneG02 <46197495+soleneG02@users.noreply.github.com>
2019-01-22 10:14:55 +0100

    Avec commandes vocales

--- app/src/main/java/com/example/solene/galidog/CommandeVocale.java
+++ app/src/main/java/com/example/solene/galidog/CommandeVocale.java
@@ -1,8 +1,14 @@
 package com.example.solene.galidog;
 
+import android.content.Context;
+import android.content.res.AssetFileDescriptor;
+import android.content.res.AssetManager;
 import android.media.MediaPlayer;
+import android.net.Uri;
 
 import com.google.android.gms.maps.model.LatLng;
+
+import java.io.IOException;
 
 public class CommandeVocale {
 
@@ -11,20 +17,24 @@ public class CommandeVocale {
     private static int id = 0;
     private LatLng coordonnees;
 
-    public CommandeVocale(String direction, LatLng coord) {
+    public CommandeVocale(String direction, LatLng coord, Context context) throws IOException {
         this.idCommande = id ++;
         this.coordonnees = coord;
         if (direction == "D") {
-            //commande = droite
+            commandeMP3 = MediaPlayer.create(context, R.raw.droite);
+            commandeMP3.start();
         }
         else if (direction == "G") {
-            //commande = gauche
+            commandeMP3 = MediaPlayer.create(context, R.raw.gauche);
+            commandeMP3.start();
         }
         else if (direction == "H") {
-            //commande = halte
+            commandeMP3 = MediaPlayer.create(context, R.raw.halte);
+            commandeMP3.start();
         }
         else if (direction == "A") {
-            //commande = autre
+            commandeMP3 = MediaPlayer.create(context, R.raw.activez);
+            commandeMP3.start();
         }
     }
 
@@ -42,6 +52,14 @@ public class CommandeVocale {
 
     public void setCoordonnees(LatLng coordonnees) {
         this.coordonnees = coordonnees;
+    }
+
+    public MediaPlayer getCommandeMP3() {
+        return commandeMP3;
+    }
+
+    public void setCommandeMP3(MediaPlayer commandeMP3) {
+        this.commandeMP3 = commandeMP3;
     }
 
     @Override

67c2b60c478e0dc7500c74ba144ea70243ca88c1
soleneG02 <46197495+soleneG02@users.noreply.github.com>
2019-01-22 16:00:33 +0100

    Update Point.java

--- app/src/main/java/com/example/solene/galidog/Point.java
+++ app/src/main/java/com/example/solene/galidog/Point.java
@@ -1,6 +1,9 @@
 package com.example.solene.galidog;
 
-public class Point {
+import android.os.Parcel;
+import android.os.Parcelable;
+
+public class Point implements Parcelable {
 
     private double latitude;
     private double longitude;
@@ -12,6 +15,24 @@ public class Point {
         this.longitude = longitude;
         this.idPoint = id ++;
     }
+
+    protected Point(Parcel in) {
+        latitude = in.readDouble();
+        longitude = in.readDouble();
+        idPoint = in.readDouble();
+    }
+
+    public static final Creator<Point> CREATOR = new Creator<Point>() {
+        @Override
+        public Point createFromParcel(Parcel in) {
+            return new Point(in);
+        }
+
+        @Override
+        public Point[] newArray(int size) {
+            return new Point[size];
+        }
+    };
 
     public double getLatitude() {
         return latitude;
@@ -38,4 +59,15 @@ public class Point {
     }
 
 
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeDouble(latitude);
+        dest.writeDouble(longitude);
+        dest.writeDouble(idPoint);
+    }
 }
